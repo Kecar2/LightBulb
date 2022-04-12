5b0eb803e60d88b08d7187a475bdd7c3921faafa
Kecar2 <kendrycarvajalsegura>
2022-04-12 10:25:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<script setup>
+import BulbLight from "./components/BulbLight.vue";
+</script>
+
+<template>
+  <h1>¡Enciende todas las luces!</h1>
+  <main class="bulbs">
+    <bulb-light necessaryClicks="1"></bulb-light>
+    <bulb-light necessaryClicks="2"></bulb-light>
+  </main>
+</template>
+
+<style>
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+.bulbs {
+  display: flex;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+  }
+
+  #app {
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
---
+++ src/components/BulbLight.vue
@@ -0,0 +1,52 @@
+
+<script>
+export default {
+    props: ["necessaryClicks"],
+    data() {
+        return {
+            cargas: this.necessaryClicks
+        }
+    },
+    methods: {
+        decrementarCargas() {
+            this.cargas--;
+        }
+    }
+
+}
+</script>
+
+<template>
+    <div v-if="cargas > 0" @click="decrementarCargas" class="bulb bulb-off">
+        <p> {{ cargas }} </p>
+    </div>
+    <div v-else class="bulb bulb-on"></div>
+</template>
+
+<style scoped>
+p {
+    font-size: 1.5rem;
+    font-weight: 700;
+}
+
+.bulb {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.bulb-off {
+    width: 265px;
+    height: 400px;
+    background: url('lightoff.png') no-repeat;
+    margin: auto;
+}
+
+.bulb-on {
+    width: 265px;
+    height: 400px;
+    background: url('lighton.png') no-repeat;
+    ;
+    margin: auto;
+}
+</style>
